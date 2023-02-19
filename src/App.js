29e1f810ecbfcce1785f7789edca33b8b7e56bb9
sub1nyun <97420319+sub1nyun@users.noreply.github.com>
2023-02-20 01:01:38 +0900

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
 import logo from './logo.svg';
-import './App.css'; // ./ -> 같은 폴더를 의미함
+import './App.css'; 
 import { useState } from 'react';
-import { useEffect } from 'react';
-// import { num } from './Sub'; -> 다른 js에서 num넘겨주면 -> {num} 형식으로 사용이 가능함 
+import { useMemo } from 'react';
 
+// useMemo => 메모라이제이션(기억)
 
 function App() {
-  const [data, setData] = useState(0);
-  //값이 변경 될때 실행해보기
-   const [search, setSearch] = useState(0);
-  const download = () => {
-    //다운로드 받고(통신)
-    let downloadData = 5; // 가정한 데이터
-    setData(downloadData);
+
+  const [list, setList] = useState([1,2,3,4]);
+  const [str, setStr] = useState("합계");
+
+  const [isKorea, setIsKorea] = useState(true);
+
+  const location = useMemo(() => {
+    return { // 오브젝트 넣음  -> {}
+      country : isKorea ? '한국' : '외국'
+    };
+  }, [isKorea]) // useEffect 에서 오래걸리는 작업을 한다면  필요할때만 호출되게 하기
+
+
+
+  const getAddResult = () => {
+    let sum = 0;
+    list.forEach(i => sum = sum + i); // 한 줄이면 {} 필요 없음 -> 리턴을 안 해도 됨
+    console.log(sum);
+    return sum; // 리턴해주면 화면에 찍힘
   }
 
-  // 실행시점 : (1) App() 함수가 최초 실행될 때 -> 마운트 될때(그림이 그려질 때)
-  // (2) ->상태 변수가 변경될 때  -> App() 재실행 -> 리턴해서 그릴 수 있기 때문에
-  // (3) 의존리스트를 관리를 할 수 있음 -> 특정 조건을 뜻함
-  // 상태변수가 변경 되어도 -> 변경 전과 동일한 값으로 변경할 경우 작동하지 않음 
-  useEffect(() => {
-    console.log("userEffect 실행됨");
-    download();
-  }, [search]); // [] -> 어디에도 의존하지 않는다는 의미 -> 최초에 한 번만 실행
-  
+ // const addResult = useMemo(()=>getAddResult(), [list]); //기억할 함수를 지정
+                                 // ▲ { } 생략을 통해서 return문 생략
+ const addResult = useMemo(() => {
+  return getAddResult()
+ }, [list]);
+
   return (
     <div>
-      <button onClick={()=>{setSearch(2)}}>검색하기</button>
-      <h1>데이터 : {data} </h1>
-      <button onClick={()=>{setData(data+1)}}>더하기</button>
+      <button onClick={() => {setStr("안녕")}}>문자 변경</button>
+      <button onClick={() => {setList([...list, 10])}}>리스트값 추가</button>
+      <div>
+        {list.map(i => <h1>{i}</h1>)}
+      </div>
+      <div>{str} : {addResult}</div>
     </div>
   );
 }
 
-export default App; // 외부에 노출할 함수
-// default는 무조건 하나만 가능
-// 추가는 export { ~~~ }
+export default App; 
+
