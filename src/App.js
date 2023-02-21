ec9d6022c62053d3207c95f013f02ed412344fce
sub1nyun <97420319+sub1nyun@users.noreply.github.com>
2023-02-22 00:06:46 +0900

    useRef와 일반 변수의 차이

--- src/App.js
+++ src/App.js
@@ -1,27 +1,64 @@
 import logo from './logo.svg';
 import './App.css'; 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useEffect } from 'react';
 
 function App() {
-  const inputRef = useRef();
+  /*
+  const [count, setCount] = useState(0);
+  const  countRef = useRef(0); // Ref는 하나의 {오브젝트이며} - current 라는 값을 갖음
 
-  useEffect(() => {
-   // console.log(inputRef);
-   inputRef.current.focus();
-  }, []);
+  const increaseCountState = ()  => {
+    setCount(count +1);
+  };
 
-  const login = () => {
-    alert(`환영합니다 ${inputRef.current.value}!`);
-    //로그인을 마친 후 다시 포커스
-    inputRef.current.focus();
+  const increaseCountRef = () => {
+    countRef.current = countRef.current +1;
+    console.log(countRef.current);
   }
-  //input의 밸류를 얻을때, e.target.value 뿐만 아니라 
-  //useRef를 써서 inputRef.current.value로
+
+    return (
+    <div>
+      <p>State: {count}</p>
+      <p>Ref: {countRef.current}</p>
+      <button onClick={increaseCountState}>State 올리세요</button>
+      <button onClick={increaseCountRef}>Ref 값 올려</button>
+    </div>
+  );
+
+  useRef => 변화는 감지해야 하지만 그 변화가 렌더링을 발생시키면 안 될경우
+
+*/
+  const [renderer, setRenderer] = useState(0);
+   const countRef = useRef(0); //다시 렌더링이 되더라도 값이 유지가 됨
+   let countVar = 0; //App()가 호출 될때마다 0으로 초기화
+
+   const increaseRef = () => {
+    countRef.current = countRef.current+1;
+   }
+
+   const increaseVar = () => {
+    countVar = countVar +1;
+   }
+
+   const doRendering = () => {
+    setRenderer(renderer + 1);
+   }
+
+   const [count, setCount] = useState(1);
+   const renderCount =useRef(1);
+
+   useEffect(() => {
+    renderCount.current = renderCount.current +1;
+   })
+
   return (
     <div>
-      <input ref={inputRef} type="text" placeholder='username'/>
-      <button onClick={login}>로그인</button>
+      <p>Ref: {countRef.current}</p>
+      <p>Var: {countVar}</p>
+      <button onClick={doRendering}>렌더!</button>
+      <button onClick={increaseRef}>Ref 올리세요</button>
+      <button onClick={increaseVar}>Var 올리세요</button>
     </div>
   );
 }
