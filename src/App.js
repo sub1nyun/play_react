4f4df088d16ca71519847f2294cd8222092d5d40
sub1nyun <97420319+sub1nyun@users.noreply.github.com>
2023-02-23 01:15:24 +0900

    components styled 및 컴포넌트화(재활용) pages 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,65 +1,37 @@
 import logo from './logo.svg';
 import './App.css'; 
-import { useRef, useState } from 'react';
-import { useEffect } from 'react';
+import styled from 'styled-components';
+
+// div~ 가상의 태그를 만들어서 지정해줌 `` 백틱 사용하는게 좋음
+// vscode-styled-components 확장 설치
+// js 파일과 css 파일이 하나로 합쳐지기에 -> 컴포넌트화 됨 -> 하나의 파일에서 관리가 가능
+const Parent = styled.div` 
+  display: flex;
+`;
+
+const ChlidBoxOne = styled.div`
+  background-color: pink;
+  width: 100px;
+  height: 100px;
+`;
+
+const ChlidBoxTwo = styled.div`
+  background-color: black;
+  width: 100px;
+  height: 100px;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: left;
+  color: red;
+`;
 
 function App() {
-  /*
-  const [count, setCount] = useState(0);
-  const  countRef = useRef(0); // Ref는 하나의 {오브젝트이며} - current 라는 값을 갖음
-
-  const increaseCountState = ()  => {
-    setCount(count +1);
-  };
-
-  const increaseCountRef = () => {
-    countRef.current = countRef.current +1;
-    console.log(countRef.current);
-  }
-
-    return (
-    <div>
-      <p>State: {count}</p>
-      <p>Ref: {countRef.current}</p>
-      <button onClick={increaseCountState}>State 올리세요</button>
-      <button onClick={increaseCountRef}>Ref 값 올려</button>
-    </div>
-  );
-
-  useRef => 변화는 감지해야 하지만 그 변화가 렌더링을 발생시키면 안 될경우
-
-*/
-  const [renderer, setRenderer] = useState(0);
-   const countRef = useRef(0); //다시 렌더링이 되더라도 값이 유지가 됨
-   let countVar = 0; //App()가 호출 될때마다 0으로 초기화
-
-   const increaseRef = () => {
-    countRef.current = countRef.current+1;
-   }
-
-   const increaseVar = () => {
-    countVar = countVar +1;
-   }
-
-   const doRendering = () => {
-    setRenderer(renderer + 1);
-   }
-
-   const [count, setCount] = useState(1);
-   const renderCount =useRef(1);
-
-   useEffect(() => {
-    renderCount.current = renderCount.current +1;
-   })
-
   return (
     <div>
-      <p>Ref: {countRef.current}</p>
-      <p>Var: {countVar}</p>
-      <button onClick={doRendering}>렌더!</button>
-      <button onClick={increaseRef}>Ref 올리세요</button>
-      <button onClick={increaseVar}>Var 올리세요</button>
-    </div>
+      <Title>헬로</Title>
+    </div>  
   );
 }
 export default App; 
